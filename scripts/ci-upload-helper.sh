bc3ca9db947b3daf6b81b0564db1f727d8b83dd3
Koen Kooi <koen@dominion.thruhere.net>
2014-02-23 10:45:37 +0100

    scripts: add ci-upload-helper-script
    This will compress extX and *.img files and drop broken symlinks.
    
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

---
+++ scripts/ci-upload-helper.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+HOST="$1"
+nightlydir="$2"
+
+# create directory on remote machines
+ssh ${HOST} "mkdir -p ${nightlydir}"
+
+# Compress raw images and extX files if needed
+for i in $(find deploy/eglibc/images/ -name "A*.ext3"; find deploy/eglibc/images/ -name "A*img" ; find deploy/eglibc/images/ -name "A*.ext2 ; find deploy/eglibc/images/ -name "A*.ext4) ; do xz -f -v -z -T0 -9 -e $i ; done
+
+# Clean up broken symlinks
+find deploy/eglibc/images/ -type l -exec sh -c "file -b {} | grep -q ^broken" \; -print | xargs rm || true
+
+# Copy over images/kernels/modules/etc
+rsync -l deploy/eglibc/images/$machine/* ${HOST}:${nightlydir}
