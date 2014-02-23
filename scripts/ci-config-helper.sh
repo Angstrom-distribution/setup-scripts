e56dc59092ea8eb6ddcd63d0a047404fdc673b6a
Koen Kooi <koen@dominion.thruhere.net>
2014-02-23 10:28:34 +0100

    scripts: add ci-config-helper script
    This sadly is specific to my Jenkins setup, but this should be made
    generic. Or absorbed into the repo based config script.
    
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

---
+++ scripts/ci-config-helper.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+if [ -e /usr/bin/getconf ] ; then
+	NUMCPU="$(getconf _NPROCESSORS_ONLN)"
+else
+	NUMCPU="4"
+fi
+
+# Use more parallelism
+sed -i -e "s:-j2:-j${NUMCPU}:" -e 's:THREADS = "2":THREADS = "4":' conf/local.conf
+
+# Point to shared download dir
+SHARED_DL_DIR="$(echo $WORKSPACE | awk -F'/angstrom-v2013.' '{print $1}')/angstrom-v2013.12-shared/downloads"
+sed -i -e s:'${OE_SOURCE_DIR}/downloads':${SHARED_DL_DIR}: oebb.sh
+
+if [ -e ${SHARED_DL_DIR}/../v2013.12-gits.tar.xz ] ; then
+	tar xf ${SHARED_DL_DIR}/../v2013.12-gits.tar.xz
+fi
+
+# Point to shared sstate-dir
+SHARED_SSTATE_DIR="$(echo $WORKSPACE | awk -F'/angstrom-v2013' '{print $1}')/angstrom-v2013.12-shared/sstate-cache"
+sed -i -e s:'${OE_BUILD_DIR}/build/sstate-cache':${SHARED_SSTATE_DIR}: oebb.sh
+
+# Freescale EULA
+echo 'ACCEPT_FSL_EULA = "1"' >> conf/local.conf
+
+# Intel EMGD
+echo 'LICENSE_FLAGS_WHITELIST += "license_emgd-driver-bin commercial"' >> conf/local.conf
+
+if [ -e /usr/bin/autogen ] ; then
+	echo 'ASSUME_PROVIDED += "autogen-native"' >> conf/local.conf
+fi
+
+if [ -e /usr/bin/svn ] ; then
+	echo 'ASSUME_PROVIDED += "subversion-native"' >> conf/local.conf
+fi
+
+# Work around guile-native build problems
+if [ "$(lsb_release -si)" = "Angstrom" ] ; then
+	if [ -e /usr/bin/guile ] ; then
+		echo 'ASSUME_PROVIDED += "guile-native"' >> conf/local.conf
+	fi
+fi
