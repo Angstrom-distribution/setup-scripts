131a34295f69e01e493f211154bcc4391a12fe94
Koen Kooi <koen@dominion.thruhere.net>
2014-07-09 08:26:49 +0200

    ci-upload-helper: ensure image directory exists
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- scripts/ci-upload-helper.sh
+++ scripts/ci-upload-helper.sh
@@ -6,6 +6,8 @@ nightlydir="$2"
 # create directory on remote machines
 ssh ${HOST} "mkdir -p ${nightlydir}"
 
+mkdir -p deploy/eglibc/images
+
 # Compress raw images and extX files if needed
 for i in $(find deploy/eglibc/images/ -name "A*.ext3"; find deploy/eglibc/images/ -name "A*img" ; find deploy/eglibc/images/ -name "A*.ext2" ; find deploy/eglibc/images/ -name "A*.ext4" ; find deploy/eglibc/images/ -name "A*sdcard" ; find deploy/eglibc/images/ -name "A*iso") ; do xz -f -v -z -T0 -9 -e $i ; done
 
