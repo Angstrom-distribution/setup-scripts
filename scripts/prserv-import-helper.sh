dc7c5250855decf7a5e58d8fc89b11ace6e60dc0
Koen Kooi <koen@dominion.thruhere.net>
2014-08-26 09:45:30 +0200

    scripts: prserv-import-helper: don't stop build on error
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- scripts/prserv-import-helper.sh
+++ scripts/prserv-import-helper.sh
@@ -5,5 +5,5 @@ if [ -e "${PRSERVFILE}" ] ; then
     echo "Using ${PRSERVFILE} to import PRs"
     cp ${PRSERVFILE} .
     . ./environment-angstrom-next
-    time bitbake-prserv-tool import $(basename ${PRSERVFILE})
+    time bitbake-prserv-tool import $(basename ${PRSERVFILE}) || true
 fi
