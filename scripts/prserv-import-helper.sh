8fe1aa9916ba1fefe5b8b187511cd670eb9dfaa3
Koen Kooi <koen@dominion.thruhere.net>
2014-12-14 09:16:48 +0100

    scripts: prserv-import-helper.sh: fix enviroment source name
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- scripts/prserv-import-helper.sh
+++ scripts/prserv-import-helper.sh
@@ -4,6 +4,6 @@ PRSERVFILE="$1"
 if [ -e "${PRSERVFILE}" ] ; then
     echo "Using ${PRSERVFILE} to import PRs"
     cp ${PRSERVFILE} .
-    . ./environment-angstrom-next
+    . ./environment-angstrom
     time bitbake-prserv-tool import $(basename ${PRSERVFILE}) || true
 fi
